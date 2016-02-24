11e56add7233eb17722fcb54dcc0fd71573d2654
Andypsamp <andypsamp10@hotmail.com>
2016-02-24 10:10:07 +0100

    First

---
+++ theme_default_1/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Default Theme 1',
+    'description': 'Default website theme to showcase customization possibilities.',
+    'category': 'Theme/Hidden',
+    'sequence': 1000,
+    'version': '1.0',
+    'author': 'Odoo S.A.',
+    'depends': ['website'],
+
+    'images': [
+        'static/description/cover.png',
+    ],
+    'application': False,
+}
